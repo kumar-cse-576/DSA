5466786f52340794e8960000b6a7399c55dbd146
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-04-23 21:54:30 +0530

    Update Sum_of_series.cpp

--- Mathematics/Sum of series/cpp/Sum_of_series.cpp
+++ Mathematics/Sum of series/cpp/Sum_of_series.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+class Solution {
+  public:
+    long long seriesSum(long long int n) {
+        //using sum of n natural numbers formula
+        return (n*(n+1)/2);
+    }
+};
+
+
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        long long int n;
+        cin>>n;
+        
+        Solution obj;
+        long long res = obj.seriesSum(n);//calling member function through object
+        
+        cout<<res<<endl;
+        
+    }
+}
