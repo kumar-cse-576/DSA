b55a693ebccdeb2e75a4915500712f5721e2a1cf
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-04-23 22:02:04 +0530

    Update Sum_of_series.java

--- Mathematics/Sum of series/java/Sum_of_series.java
+++ Mathematics/Sum of series/java/Sum_of_series.java
@@ -1,1 +1,28 @@
+import java.io.*;
+import java.util.*;
 
+class Sum_of_series {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            long res = obj.seriesSum(n);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+
+class Solution {
+    public static long seriesSum(int n) {
+        return ((long)n)*((long)(n+1))/2 ;//type casting integer to long to handle large values
+    }
+}
