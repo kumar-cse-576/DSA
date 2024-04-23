3ae145d49210f5e6074714765a3e1439eae120c3
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-04-23 22:33:41 +0530

    Create find_median.cpp

---
+++ Mathematics/Find the median/find_median.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+	public:
+		int find_median(vector<int> v)
+		{
+		    sort(v.begin(),v.end());// initially the sorting the vector
+		    int n=v.size();
+		    if(n%2==0){
+            //if length is even returning the average of middle two elements
+		        return (v[(n/2)-1]+v[n/2])/2;
+		    }
+		    return (v[n/2]);//else returning the middle element
+		}
+};
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
