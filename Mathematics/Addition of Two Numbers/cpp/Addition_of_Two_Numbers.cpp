13f5db70e675459c332864b8a213e8b3143db84b
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-05-10 14:40:46 +0530

    Update Addition_of_Two_Numbers.cpp

--- Mathematics/Addition of Two Numbers/cpp/Addition_of_Two_Numbers.cpp
+++ Mathematics/Addition of Two Numbers/cpp/Addition_of_Two_Numbers.cpp
@@ -1,3 +1,19 @@
+
+/*
+Question :
+Given two numbers A and B. Your task is to return the sum of A and B.
+
+Example 1:
+Input:
+A = 1, B = 2
+Output:
+3
+Explanation:
+Addition of 1 and 2 is 3.
+*/
+
+
+
 //{ Driver Code Starts
 #include<bits/stdc++.h> 
 using namespace std; 
