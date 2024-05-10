a807023dad4c5986288ac0972a214fad93ed67a7
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-05-10 14:42:14 +0530

    Update Armstrong.cpp

--- Mathematics/ArmstrongNumber/cpp/Armstrong.cpp
+++ Mathematics/ArmstrongNumber/cpp/Armstrong.cpp
@@ -1,3 +1,23 @@
+
+/*
+Question:
+For a given 3 digit number, find whether it is armstrong number or not. An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to the number itself. Return "Yes" if it is a armstrong number else return "No".
+NOTE: 371 is an Armstrong number since 3^3 + 7^3 + 1^3 = 371
+
+Example 1:
+
+Input: N = 153
+Output: "Yes"
+Explanation: 153 is an Armstrong number
+since 1^3 + 5^3 + 3^3 = 153.
+Hence answer is "Yes".
+
+*/
+
+
+
+
+
 //{ Driver Code Starts
 // Initial Template for C++
 #include <bits/stdc++.h>
