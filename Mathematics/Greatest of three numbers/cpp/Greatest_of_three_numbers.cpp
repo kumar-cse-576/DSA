f1973d411803896d0c04af1f3dd6ff3aa50ee41e
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-05-10 14:48:35 +0530

    Update Greatest_of_three_numbers.cpp

--- Mathematics/Greatest of three numbers/cpp/Greatest_of_three_numbers.cpp
+++ Mathematics/Greatest of three numbers/cpp/Greatest_of_three_numbers.cpp
@@ -1,3 +1,15 @@
+
+/*
+Question:
+Given 3 numbers A, B and C. Find the greatest number among them.
+
+Example 1:
+Input: A = 10, B = 3, C = 2
+Output: 10
+Explanation:
+10 is the greatest among the three.
+*/
+
 //{ Driver Code Starts
 // Initial Template for C++
 
