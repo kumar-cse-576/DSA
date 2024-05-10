feabb9f240a8d4e5ebc89c85f90cc4a71911fcd5
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-05-10 14:43:14 +0530

    Update Average_in_a_stream.cpp

--- Mathematics/Average in a stream/cpp/Average_in_a_stream.cpp
+++ Mathematics/Average in a stream/cpp/Average_in_a_stream.cpp
@@ -1,3 +1,21 @@
+
+/*
+Question:
+Given a stream of incoming numbers, find average or mean of the stream at every point.
+
+Example 1:
+Input:
+n = 5
+arr[] = {10, 20, 30, 40, 50}
+Output: 10.00 15.00 20.00 25.00 30.00 
+Explanation: 
+10 / 1 = 10.00
+(10 + 20) / 2 = 15.00
+(10 + 20 + 30) / 3 = 20.00
+And so on.
+
+*/
+
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
