46e5dfd49f0a92d849394f7773e1d59976669e9c
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-05-10 14:57:07 +0530

    Update Simple_Interest.cpp

--- Mathematics/Simple Interest/cpp/Simple_Interest.cpp
+++ Mathematics/Simple Interest/cpp/Simple_Interest.cpp
@@ -1,3 +1,20 @@
+
+/*
+Question:
+Given three integers P,R and T, denoting Principal, Rate of Interest and Time period respectively.Compute the simple Interest.
+
+Example 1:
+Input:
+P=100
+R=20
+T=2
+Output:
+40.00
+Explanation:
+The simple interest on 100 at a rate
+of 20% across 2 time periods is 40.
+*/
+
 //{ Driver Code Starts
 // Initial Template for C++
 
