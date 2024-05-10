19f00b0cf54a97515e11af2ad26f7b118ccf0d01
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-05-10 14:58:18 +0530

    Update Sum_Palindrome.cpp

--- Mathematics/Sum Palindrome/cpp/Sum_Palindrome.cpp
+++ Mathematics/Sum Palindrome/cpp/Sum_Palindrome.cpp
@@ -1,3 +1,14 @@
+
+/*
+Question:
+Given a number, reverse it and add it to itself unless it becomes a palindrome or number of iterations becomes more than 5.
+
+Example 1:
+Input: n = 23
+Output: 55 
+Explanation: reverse(23) = 32,then 32+23= 55 which is a palindrome. 
+*/
+
 //{ Driver Code Starts
 // Initial Template for C++
 #include <bits/stdc++.h>
