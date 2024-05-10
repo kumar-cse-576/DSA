b7c595099d61132ddb171cbb5d7ed0bcca8c5923
Kumar <149708316+kumar-cse-576@users.noreply.github.com>
2024-05-10 14:50:20 +0530

    Update Multiplication_Table.cpp

--- Mathematics/Multiplication Table/cpp/Multiplication_Table.cpp
+++ Mathematics/Multiplication Table/cpp/Multiplication_Table.cpp
@@ -1,3 +1,17 @@
+
+/*
+Question:
+Create the multiplication table of a given number N and return the table as an array.
+
+Example 1:
+Input:
+N = 9
+Output:
+9 18 27 36 45 54 63 72 81 90
+Explanation:
+The table of 9 is the output whose 1st 
+term is 9 and the 10th term is 90.
+*/
 //{ Driver Code Starts
 #include<bits/stdc++.h> 
 using namespace std; 
